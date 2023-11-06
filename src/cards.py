395dfeb9c6c32f912e41e99381f5a00f2e01f3f2
Rahul Vikram <vikramr@oregonstate.edu>
2023-11-05 16:47:42 -0800

    card class split into module

--- src/cards.py
+++ src/cards.py
@@ -1,6 +1,8 @@
 import random
 import sys
 
+from card import Card
+
 # TODO: define Deck class
 class Deck:
     def __init__(self) -> None:
@@ -14,30 +16,6 @@ class Pile:
     # draw a card from the pile
     def draw(self):
         pass
-
-class Card:
-    def __init__(self, value, color, suit, faceup):
-        self.value = value
-        self.color = color
-        self.suit = suit
-        self.faceup = faceup
-
-    # Plays the card from the current deck
-    def play(self, start_hand, end, faceup):
-        try:
-            end.append(self) # adds the card to the pile (end destination)
-            start_hand.remove(self) # removes the card from the hand of the player who played
-            faceup = True; # card has been played, so the card is now face up
-        except:
-            pass
-
-    # reveals the card
-    def show(self):
-        # card can only be shown if it's not already being shown
-        if not self.faceup:
-            print(f"{self.value} of {self.suit}") # e.g. (4 of spades)
-            self.faceup = True;
-        
 
 # initializes sorted deck of cards
 def init_deck():
