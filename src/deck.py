4fdaea8e06b715b88b9887d76a173790c2c0ae5c
rahulvikram <rahulvikram479@gmail.com>
2023-11-26 21:54:58 -0800

    deck dealing func

--- src/deck.py
+++ src/deck.py
@@ -1,9 +1,11 @@
 # required modules
 import random
+import sys
 
 # import helper functions
-from helpers.values import val_dict
-from helpers.sort_help import value_func, suit_func
+sys.path.append('/Users/rahul/OneDrive - Oregon State University/Documents/Coding/projects/card-game/helpers/')
+from sort_help import value_func, suit_func
+from values import val_dict # decksort alg: converts AJQK into respective num values
 
 # import classes
 from card import Card 
@@ -28,17 +30,31 @@ class Deck:
             card.print_card()
 
 
-    # deals cards to players
-    def deal(self, player_count):
-        players = []
-        for i in range(player_count):
-            pass
-        
-        return players
-        
-
     def shuffle(self):
         random.shuffle(self.cards)                                                                                                                                                                                                                                                                             
+
+    # deals cards to players
+    def deal(self, player_count, card_count):
+        players = []
+
+        # makes sure the cards to deal isn't more than the total cards in the deck
+        if player_count*card_count > len(self.cards):
+            print("Error: Not enough cards in deck.\n")
+            sys.exit()
+        else: # if enough cards are in deck
+            self.shuffle()
+
+            # create new players, add them to player list, and give them cards from shuffled deck
+            for i in range(player_count):
+                new_player = Player()
+                players.append(new_player)
+
+
+
+
+
+        return players
+        
 
     def draw(self):
         pass
@@ -49,6 +65,6 @@ class Deck:
     def is_sorted(self):
         pass
 
+
 deck = Deck()
-
-
+deck.deal(4, 10)
