4b58e2c183f7f3014d22a25c421cf5d16420c2da
rahulvikram <rahulvikram479@gmail.com>
2023-11-28 00:01:20 -0800

    🖨 print hand function

--- src/player.py
+++ src/player.py
@@ -1,5 +1,9 @@
-
 class Player:
     def __init__(self):
         self.hand = [] # new player's hand is initially empty, will be populated later on
         pass
+
+    def print_hand(self):
+        for card in self.hand:
+            card.print_card()
+        print('\n')
