fdc330d4781d18ef813cb234ebaa11dc75bb74ec
Rahul Vikram <vikramr@oregonstate.edu>
2023-12-13 13:23:47 -0800

    � constants and help functions for game

---
+++ helpers/constants.py
@@ -0,0 +1,10 @@
+# ------------------- constants used in main.py ------------------- #
+
+# background colors
+WHITE = (255, 255, 255)
+BLACK = (13, 13, 13)
+
+WIDTH = 1280
+HEIGHT = 720
+CENTER = (WIDTH/2, HEIGHT/2)
+
